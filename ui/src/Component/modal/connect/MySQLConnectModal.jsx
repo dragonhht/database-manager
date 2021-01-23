1781a96c38c2a9f55f3c36c8b9c6ba8c43a3bc86
dragonhht <dragon_hht@163.com>
2021-01-23 21:33:55 +0800

    mysql

--- ui/src/Component/modal/connect/MySQLConnectModal.jsx
+++ ui/src/Component/modal/connect/MySQLConnectModal.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import { Modal, Button } from 'antd';
+import { Modal, Button, Tabs, Form, Input, Checkbox } from 'antd';
+const { TabPane } = Tabs;
+
+import cssObj from '@/Component/css/ConnectModal.less'
 
 class MySQLConnectModal extends React.Component {
 
@@ -15,6 +18,19 @@ class MySQLConnectModal extends React.Component {
         ...props.data
       }
     }
+  }
+
+  // 初始值
+  initialValues = {
+    id: '',
+    type: 'mysql',
+    savePwd: false,
+    userName: '',
+    pssword: '',
+    port: 3306,
+    ip: '',
+    database: '',
+    charset: 'utf-8'
   }
 
 
@@ -33,9 +49,37 @@ class MySQLConnectModal extends React.Component {
           确定
       </Button>,
       ]}
+      bodyStyle={{ padding: '5px' }}
     >
-
-    </Modal>
+      <Tabs type="card">
+        <TabPane tab="常规" key="1">
+          <Form name="Basic" labelAlign="right" labelCol={{ span: 4 }} wrapperCol={{ span: 16 }} initialValues={this.initialValues}>
+            <Form.Item label="连接名:" name="name"
+              rules={[{ required: true, message: "请输入连接名" }]}>
+              <Input />
+            </Form.Item>
+            <Form.Item label="IP地址" name="ip"
+              rules={[{ required: true, message: "请输入IP地址" }]}>
+              <Input />
+            </Form.Item>
+            <Form.Item label="端口" name="port" wrapperCol={{ span: 4 }}
+              rules={[{ required: true, message: "请输入端口" }]}>
+              <Input />
+            </Form.Item>
+            <Form.Item wrapperCol={{ span: 12 }} label="用户名" name="userName"
+              rules={[{ required: true, message: "请输入用户名" }]}>
+              <Input />
+            </Form.Item>
+            <Form.Item wrapperCol={{ span: 12 }} label="密码" name="password" >
+              <Input />
+            </Form.Item>
+            <Form.Item wrapperCol={{ offset: 4 }} name="savePwd" valuePropName="checked">
+              <Checkbox>记住密码</Checkbox>
+            </Form.Item>
+          </Form>
+        </TabPane>
+      </Tabs>
+    </Modal >
   }
 
   // 确认
